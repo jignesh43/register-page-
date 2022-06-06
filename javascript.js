1acb1a860e1df041645b4c909d5785024690d256
jignesh_joshi <joshijignesh885@gmail.com>
2022-06-06 02:07:01 -0600

    register page

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let text =document.getElementById("form");
+text.style.textAlign ="center"
+text.style.color ="red"
+let fr = document.getElementById("text");
+let frc = document.getElementById("last");
+let frc1 = document.getElementById("pwd");
+let frc2 = document.getElementById("password");
+let frc3 = document.getElementById("Email");
+document.getElementById("form").addEventListener("click",abc);
+
+function abc(){
+    fr.style.backgroundColor="yellow";
+   frc.style.backgroundColor="yellow";
+    frc1.style.backgroundColor="yellow";
+    frc2.style.backgroundColor="yellow";
+    frc3.style.backgroundColor="yellow";
+}
+
+
